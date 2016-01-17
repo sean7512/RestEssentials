6e9658a584808a40e55fe6a9859ebfabdb365568
Sean <sean.kosanovich@agilex.com>
2016-01-17 11:15:13 -0500

    Adding support for DELETE calls, ability to accept self signed certificates, and chasing the timeout length

--- Source/RestController.swift
+++ Source/RestController.swift
@@ -27,7 +27,7 @@ internal extension JSON {
             return nil
         }
     }
-    
+
     func createNSData() throws -> NSData {
         return try NSJSONSerialization.dataWithJSONObject(jsonValue.value, options: [])
     }
@@ -57,39 +57,65 @@ public struct RestOptions {
     /// An optional set of HTTP Headers to send with the call.
     public var httpHeaders: [String : String]?
 
+    /// The amount of time in `seconds` until the request times out.
+    public var requestTimeoutSeconds = RestController.kDefaultRequestTimeout
 }
 
 /// Allos users to create HTTP REST networking calls that deal with JSON.
-public class RestController {
-    
+///
+/// **NOTE:** If running on iOS 9.0+ then ensure to configure `App Transport Security` appropriately.
+public class RestController : NSObject, NSURLSessionDelegate {
+
     private static let kPostType = "POST"
     private static let kGetType = "GET"
     private static let kPutType = "PUT"
+    private static let kDeleteType = "DELETE"
     private static let kJsonType = "application/json"
     private static let kContentType = "Content-Type"
     private static let kAcceptKey = "Accept"
     private static let kDefaultRequestTimeout = 60 as NSTimeInterval
-    
+
     private let url: NSURL
-    
+    private var session: NSURLSession
+
+    /// If set to *true*, then self signed SSL certificates will be accepted from the **SAME** host only.
+    ///
+    /// If you are making a request to *https://foo.com* and you get redirected to *https://bar.com* (where bar.com uses a self signed SSL certificate), then the request will fail; as the SSL host of *bar.com* does not match the intended host of *foo.com*.
+    ///
+    /// **NOTE:** If running on iOS 9.0+ then ensure to configure `App Transport Security` appropriately.
+    public var acceptSelfSignedCertificate = false
+
     private init(_ url: NSURL) {
         self.url = url
+        self.session = NSURLSession.sharedSession()
     }
 
     /// Creates a new `RestController` for the given URL endpoint.
     ///
+    /// **NOTE:** If running on iOS 9.0+ then ensure to configure `App Transport Security` appropriately for the server.
     /// - parameter urlString: The URL of the server to send requests to.
     /// - returns: If the given URL string represents a valid `NSURL`, then a `RestController` will be returned; it not then `nil` will be returned.
     public static func createFromURLString(urlString: String) -> RestController? {
         if let validURL = NSURL(string: urlString) {
-            return RestController(validURL)
+            let restController = RestController(validURL)
+            restController.session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration(), delegate: restController, delegateQueue: nil)
+            return restController
         }
-        
+
         return nil
     }
-    
+
+    @objc public func URLSession(session: NSURLSession, didReceiveChallenge challenge: NSURLAuthenticationChallenge, completionHandler: (NSURLSessionAuthChallengeDisposition, NSURLCredential?) -> Void) {
+        if(acceptSelfSignedCertificate && challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust && challenge.protectionSpace.host == url.host) {
+            let credential = NSURLCredential(forTrust: challenge.protectionSpace.serverTrust!)
+            completionHandler(.UseCredential, credential);
+        } else {
+            completionHandler(.PerformDefaultHandling, nil)
+        }
+    }
+
     private func makeCall(httpMethod: String, withJSONData json: JSON?, withOptions options: RestOptions, withCallback callback: (Result<(data: NSData, response: NSHTTPURLResponse)>) -> ()) throws {
-        let request = NSMutableURLRequest(URL: url, cachePolicy: .UseProtocolCachePolicy, timeoutInterval: RestController.kDefaultRequestTimeout)
+        let request = NSMutableURLRequest(URL: url, cachePolicy: .UseProtocolCachePolicy, timeoutInterval: options.requestTimeoutSeconds)
         request.HTTPMethod = httpMethod
 
         request.setValue(RestController.kJsonType, forHTTPHeaderField: RestController.kAcceptKey)
@@ -98,23 +124,23 @@ public class RestController {
                 request.setValue(httpHeaderValue, forHTTPHeaderField: httpHeaderKey)
             }
         }
-        
+
         if let jsonObj = json {
             request.setValue(RestController.kJsonType, forHTTPHeaderField: RestController.kContentType)
             let jsonData = try jsonObj.createNSData()
             request.HTTPBody = jsonData
         }
-        
+
         UIApplication.sharedApplication().networkActivityIndicatorVisible = true
-        
-        NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) -> Void in
+
+        session.dataTaskWithRequest(request) { (data, response, error) -> Void in
             UIApplication.sharedApplication().networkActivityIndicatorVisible = false
 
             if let err = error {
                 callback(.Failure(err))
                 return
             }
-            
+
             guard let httpResponse = response as? NSHTTPURLResponse else {
                 callback(.Failure(NetworkingError.BadResponse))
                 return
@@ -131,14 +157,14 @@ public class RestController {
             }
 
             callback(.Success(data: returnedData, response: httpResponse))
-        }.resume()
+            }.resume()
     }
-    
+
     private func makeCallForJSONData(httpMethod: String, withJSONData json: JSON?, withOptions options: RestOptions, withCallback callback: (Result<JSON>) -> ()) throws {
         try makeCall(httpMethod, withJSONData: json, withOptions: options) { (result) -> () in
             do {
                 let jsonData = try result.value().data
-                
+
                 if let jsonObj = JSON(fromData: jsonData) {
                     callback(.Success(jsonObj))
                 } else {
@@ -235,5 +261,29 @@ public class RestController {
     public func getIgnoringResponseData(withOptions options: RestOptions = RestOptions(), withCallback callback: (Result<NSHTTPURLResponse>) -> ()) {
         // can only throw if serializing json
         try! makeCallForNoResponseData(RestController.kGetType, withJSONData: nil, withOptions: options, withCallback: callback)
+    }
+
+    /// Performs a DELETE request to the server, capturing the `JSON` response from the server.
+    ///
+    /// Note: This is an **asynchronous** call and will return immediately.  The network operation is done in the background.
+    ///
+    /// - parameter options: An **optional** parameter of a `RestOptions` struct containing any header fields to include with the call or a different expected status code.
+    /// - parameter callback: Called when the network operation has ended, giving back a Boxed `Result<JSON>` indicating the success of the call with the returned data. Note: The callback is **NOT** called on the main thread.
+    /// - returns: Nothing.
+    public func delete(withOptions options: RestOptions = RestOptions(), withCallback callback: (Result<JSON>) -> ()) {
+        // can only throw if serializing json
+        try! makeCallForJSONData(RestController.kDeleteType, withJSONData: nil, withOptions: options, withCallback: callback)
+    }
+
+    /// Performs a DELETE request to the server, while ignoring any response sent back from the server.
+    ///
+    /// Note: This is an **asynchronous** call and will return immediately.  The network operation is done in the background.
+    ///
+    /// - parameter options: An **optional** parameter of a `RestOptions` struct containing any header fields to include with the call or a different expected status code.
+    /// - parameter callback: Called when the network operation has ended, giving back a Boxed `Result<NSHTTPURLResponse>` indicating the success of the call. Note: The callback is **NOT** called on the main thread.
+    /// - returns: Nothing.
+    public func deleteIgnoringResponseData(withOptions options: RestOptions = RestOptions(), withCallback callback: (Result<NSHTTPURLResponse>) -> ()) {
+        // can only throw if serializing json
+        try! makeCallForNoResponseData(RestController.kDeleteType, withJSONData: nil, withOptions: options, withCallback: callback)
     }
 }
